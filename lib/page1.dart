7ca59eaa5bb427cf6e09036294539dbde996abac
denaaay <dfahrony7@gmail.com>
2022-04-10 11:21:17 +0700

    udhh bangg

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class page1 extends StatefulWidget {
@@ -13,21 +15,76 @@ class _page1State extends State<page1> {
     final tinggi = MediaQuery.of(context).size.height;
     final lebar = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.all(Radius.circular(8))),
-              width: lebar * 0.9,
-              height: tinggi * 0.9,
-              child: Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula mollis ligula, sit amet gravida diam fringilla ac. Donec in nisl nec sem molestie molestie. Curabitur sit amet enim sollicitudin, mattis eros non, euismod justo. Nulla ac convallis odio. Vestibulum hendrerit dignissim enim vel porta. Etiam ac magna mi. Proin iaculis orci dignissim, mollis massa a, pretium velit. Pellentesque facilisis dui eget nulla iaculis, non facilisis odio imperdiet. Sed viverra auctor lorem.Sed dolor augue, iaculis sed dolor nec, mollis pulvinar ligula. Duis pharetra id risus et elementum. Integer finibus justo odio, et vestibulum purus ultricies a. Sed lorem arcu, convallis quis ex at, ornare gravida nisl. Proin tincidunt diam consequat mi sollicitudin rhoncus. Proin fermentum sem quis quam blandit dapibus. Pellentesque a venenatis nulla.Donec pellentesque feugiat purus pharetra luctus. Phasellus consectetur velit non consectetur vulputate. Sed non tellus vitae felis luctus blandit faucibus mattis risus. Nullam malesuada accumsan mauris. Proin luctus vitae nulla consequat tincidunt. Vestibulum venenatis aliquam turpis eget scelerisque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla erat odio, venenatis vel consectetur ut, gravida a dolor. In non lorem pellentesque, suscipit lacus ut, efficitur purus. Sed ut ex ut turpis accumsan aliquam.Sed sed neque sit amet nibh tincidunt rutrum aliquet eu orci. Aenean tincidunt nisi eget odio gravida, eu placerat libero tincidunt. In ac risus nec risus posuere feugiat ultricies ut nisi. Aliquam cursus tellus id augue placerat sodales. Maecenas ut ultrices ex, ac iaculis lorem. Curabitur ultrices efficitur neque. In ac libero commodo, aliquam lectus vitae, placerat lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In hac habitasse platea dictumst.Vestibulum mollis pharetra elit, sed laoreet neque aliquam sit amet. Pellentesque commodo eros odio, at consectetur nulla placerat a. Integer sed tellus ipsum. Sed imperdiet ligula erat, a interdum justo imperdiet ac. Duis ultricies quam ac elit consectetur condimentum. Phasellus eget dui libero. Duis vitae nibh viverra, tincidunt orci sit amet, cursus libero. Phasellus ac gravida augue, vel imperdiet ipsum. Nullam sed ante eu diam venenatis imperdiet.")),
-        ],
-      )),
-    );
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            "Cerita Aku Pusing",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              height: tinggi * 0.015,
+              width: lebar * 10,
+              color: Color(0xffe1e1e1),
+            ),
+            Container(
+              child: const Image(
+                  fit: BoxFit.cover, image: AssetImage("image/lulu.jpg")),
+              height: tinggi * 0.35,
+              width: lebar * 10,
+            ),
+            Container(
+              height: tinggi * 0.015,
+              width: lebar * 10,
+              color: Color(0xffe1e1e1),
+            ),
+            // ElevatedButton(
+            //     onPressed: () {},
+            //     child: Padding(
+            //       padding: const EdgeInsets.symmetric(
+            //           vertical: 15.0, horizontal: 10),
+            //       child: Text("Play", style: TextStyle(color: Colors.black)),
+            //     )),
+            Container(
+                child: Text(
+                  "Play Audio Text...",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      height: 3.5,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic),
+                ),
+                height: tinggi * 0.09,
+                color: Colors.white),
+            Container(
+              height: tinggi * 0.015,
+              width: lebar * 10,
+              color: Color(0xffe1e1e1),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Text(
+                  "Dalam perkembangan teknologi saat ini informasi sangat rentan terhadap serangan dan gangguan dari pihak lain. Saat pengirim dan penerima bertukar informasi melalui SMS, Email atau jenis chatting lainnya, lalu lintas informasi tersebut tidaklah terjamin keamanannya contohnya serangan man in the middle, seseorang dapat membaca, menyisipkan, ataupun mengubah data antara dua pihak yang sedang bertukar informasi atau berkomunikasi, seseorang tersebut menempatkan dirinya di tengah pembicaraan dan menyamar sebagai orang lain (M. Mustari, 2010).",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(height: 2, fontSize: 16),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Text(
+                  "Salah satu teknik pengamanan dalam penyampaian informasi adalah dengan melakukan enkripsi dan dekripsi terhadap informasi tersebut, yang dikenal dengan teknik kriptografi. Kritografi adalah ilmu seni untuk menjaga kerahasian dan keamanan suatu data atau informasi ketika informasi dikirim dari suatu tempat ke tempat yang lain (Ariyus, 2008). Prinsip dasar pengamanan kriptografi adalah proses enkripsi dan dekripsi.",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(height: 2, fontSize: 16),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
