1f0de32043bd41c900147d1456251c778807ba8b
syreboy <80834548+syreboy@users.noreply.github.com>
2022-04-10 14:53:08 +0700

    tambahan audio

--- lib/page1.dart
+++ lib/page1.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
-
+import 'dart:typed_data';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class page1 extends StatefulWidget {
   const page1({Key? key}) : super(key: key);
@@ -10,7 +12,60 @@ class page1 extends StatefulWidget {
 }
 
 class _page1State extends State<page1> {
+  int maxduration = 100;
+  int currentpos = 0;
+  String textHolder = "Speed: Normal";
+  double speed = 1;
+  String currentpostlabel = "00:00";
+  String maxdurationlabel = "00:00";
+  String audioasset = "audio/STORY1.mp3";
+  bool isplaying = false;
+  bool audioplayed = false;
+  late Uint8List audiobytes;
+
+  AudioPlayer player = AudioPlayer();
   @override
+  void initState() {
+    Future.delayed(Duration.zero, () async {
+      ByteData bytes = await rootBundle.load(audioasset);
+      audiobytes =
+          bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes);
+
+      player.onDurationChanged.listen((Duration d) {
+        maxduration = d.inMilliseconds;
+
+        int mhours = Duration(milliseconds: maxduration).inHours;
+        int mminutes = Duration(milliseconds: maxduration).inMinutes;
+        int mseconds = Duration(milliseconds: maxduration).inSeconds;
+
+        int hhours = mhours;
+        int hminutes = mminutes - (mhours * 60);
+        int hseconds = mseconds - (mminutes * 60 + mhours * 60 * 60);
+
+        maxdurationlabel = "$hhours:$hminutes:$hseconds";
+
+        setState(() {});
+      });
+
+      player.onAudioPositionChanged.listen((Duration p) {
+        currentpos = p.inMilliseconds;
+
+        int shours = Duration(milliseconds: currentpos).inHours;
+        int sminutes = Duration(milliseconds: currentpos).inMinutes;
+        int sseconds = Duration(milliseconds: currentpos).inSeconds;
+
+        int rhours = shours;
+        int rminutes = sminutes - (shours * 60);
+        int rseconds = sseconds - (sminutes * 60 + shours * 60 * 60);
+
+        currentpostlabel = "$rhours:$rminutes:$rseconds";
+
+        setState(() {});
+      });
+    });
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     final tinggi = MediaQuery.of(context).size.height;
     final lebar = MediaQuery.of(context).size.width;
@@ -41,6 +96,47 @@ class _page1State extends State<page1> {
               width: lebar * 10,
               color: Color(0xffe1e1e1),
             ),
+            Container(
+              height: lebar * 0.6,
+              child: ElevatedButton(
+                onPressed: () async {
+                  if (!isplaying && !audioplayed) {
+                    int result = await player.playBytes(audiobytes);
+                    if (result == 1) {
+                      //play success
+                      setState(() {
+                        isplaying = true;
+                        audioplayed = true;
+                      });
+                    } else {
+                      print("Error while playing audio.");
+                    }
+                  } else if (audioplayed && !isplaying) {
+                    int result = await player.resume();
+                    if (result == 1) {
+                      //resume success
+                      setState(() {
+                        isplaying = true;
+                        audioplayed = true;
+                      });
+                    } else {
+                      print("Error on resume audio.");
+                    }
+                  } else {
+                    int result = await player.pause();
+                    if (result == 1) {
+                      //pause success
+                      setState(() {
+                        isplaying = false;
+                      });
+                    } else {
+                      print("Error on pause audio.");
+                    }
+                  }
+                },
+                child: null,
+              ),
+            ),
             // ElevatedButton(
             //     onPressed: () {},
             //     child: Padding(
@@ -48,22 +144,22 @@ class _page1State extends State<page1> {
             //           vertical: 15.0, horizontal: 10),
             //       child: Text("Play", style: TextStyle(color: Colors.black)),
             //     )),
-            Container(
-                child: Text(
-                  "Play Audio Text...",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      height: 3.5,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic),
-                ),
-                height: tinggi * 0.09,
-                color: Colors.white),
-            Container(
-              height: tinggi * 0.015,
-              width: lebar * 10,
-              color: Color(0xffe1e1e1),
-            ),
+            // Container(
+            //     child: Text(
+            //       "Play Audio Text...",
+            //       textAlign: TextAlign.center,
+            //       style: TextStyle(
+            //           height: 3.5,
+            //           fontWeight: FontWeight.bold,
+            //           fontStyle: FontStyle.italic),
+            //     ),
+            //     height: tinggi * 0.09,
+            //     color: Colors.white),
+            // Container(
+            //   height: tinggi * 0.015,
+            //   width: lebar * 10,
+            //   color: Color(0xffe1e1e1),
+            // ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
